fda4e2786ce7c07e9cb6d2889079b9c606e9c498
jchillerup <391160+jchillerup@users.noreply.github.com>
2025-09-03 06:23:09 -0400

    The current user must be a member of the docker group

--- set-up-kali.sh
+++ set-up-kali.sh
@@ -3,4 +3,6 @@
 sudo apt-get update
 sudo apt install -y build-essential autoconf libssl-dev libyaml-dev zlib1g-dev libffi-dev libgmp-dev rustc docker.io
 
+sudo gpasswd -a $USER docker
+
 mise config experimental true
